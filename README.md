91129e03bf2b5469106ad67c5c4a7f9340029fb6
CakeAL <1742507190@qq.com>
2023-07-14 12:50:20 +0800

    push

---
+++ README.md
@@ -0,0 +1,18 @@
+# 瞭望新闻(LookoutNews)网(站)
+
+## 说明
+贝壳大二Java Web小学期，个人使用两天（23年7月12日和13日）做的一个非常多漏洞非常简陋的网站。  
+仅供参考。  
+
+数据库新闻表内容爬取：https://github.com/hunter-lee1/guanchazhe_spider/tree/master    
+
+## 开发环境及工具
+1. 后端：Java 8 + Mybatis，pagehelper，单元测试：junit     
+2. 前端：JSP + jQuery  
+3. 数据库：MySQL  
+4. 工具：Idea，Tomcat，Maven  
+5. 前端UI组件库：[Layui](https://layui.dev/)  
+
+## 注意
+> 请更改action里面的`SendSmsServlet.java`文件的access key和secret，否则可能无法启动。  
+> 数据库使用Mysql。
--- src/main/java/com/lookoutnews/action/SendSmsServlet.java
+++ src/main/java/com/lookoutnews/action/SendSmsServlet.java
@@ -22,9 +22,9 @@ public class SendSmsServlet extends HttpServlet {
     public static com.aliyun.dysmsapi20170525.Client createClient(String accessKeyId, String accessKeySecret) throws Exception {
         com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
                 // 必填，您的 AccessKey ID
-                .setAccessKeyId("LTAI5tAdcK2UL6DrKwrwjoot")
+                .setAccessKeyId("您的 AccessKey ID")
                 // 必填，您的 AccessKey Secret
-                .setAccessKeySecret("woThiHKYKum0PLmfThcQQRkeCBS7xw");
+                .setAccessKeySecret("AccessKey Secret");
         // 访问的域名
         config.endpoint = "dysmsapi.aliyuncs.com";
         return new com.aliyun.dysmsapi20170525.Client(config);
